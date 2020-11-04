60d40e1e97ec9d82faeedc01c0b1293d38d0ce37
Irena <irena.podgurski@gmail.com>
2020-11-04 02:50:10 +0000

    adding trash to treasure assignment

---
+++ trash-to-treasure.js
@@ -0,0 +1,18 @@
+function smartGarbage(trash, bins) {
+  switch (trash) {
+    case "waste":
+      bins.waste += 1;
+      break;
+    case "recycling":
+      bins.recycling += 1;
+      break;
+    case "compost":
+      bins.compost += 1;
+      break;
+  }
+  return bins;
+ 
+}
+
+console.log(smartGarbage("waste", { waste: 4, recycling: 2, compost: 5 }));
+
